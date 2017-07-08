567a01fd0adc55b0051ac58eaf3b9f4b0598925e
xiangnancode <xiangnan.g12@gmail.com>
2017-07-07 21:11:02 -0400

    remove unused files

--- setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(
-	name = "Xiangnan Gong",
-	version = "1.0",
-	description = "Trading Simulator",
-	executables = [Executable("main.py", base = "Win32GUI")])
